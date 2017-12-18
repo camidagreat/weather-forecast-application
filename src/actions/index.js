b52ddc69e37ab3eb9a28e2b50408d79993a61996
camidagreat <lbidigarecurtis@gmail.com>
2017-12-18 09:27:01 -0500

    added Sparklines to the pressure, humidity, and temp fields (graphics, average line, as well as a google maps integration for the lng/lat of the location of each city.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
-const API_KEY = '43dd43e14be6207c4f87160c7d2a4aa1'
+import axios from 'axios';
+
+const API_KEY = '43dd43e14be6207c4f87160c7d2a4aa1';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
